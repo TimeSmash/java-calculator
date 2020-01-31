856e4005f25616117c9b9308f43441b244eac7d2
TimeSmash <mcumm64@gmail.com>
2020-01-30 22:32:15 -0500

    First commit

---
+++ java-calc.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calc {
+    // instance fields
+    int num1;
+    int num2;
+    
+    // constructor method
+    public Calc(int firstNum, int secondNum) {
+      num1 = firstNum;
+      num2 = secondNum;
+    }
+
+    //class methods
+
+    //main
+    public static void main(String[] args) {
+      
+  
+      System.out.println(lemonadeStand);
+      System.out.println(cookieShop);
+    }
+}
